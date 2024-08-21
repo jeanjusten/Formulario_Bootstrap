08f19347488a619e68ff73d4d823bc96658c78a0
Jean Justen <jean.cjm@hotmail.com>
2024-08-21 11:43:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // Plugin de máscara de validação JQuery
+    $("#tel").mask("(00)00000-0000", {
+        placeholder: "(00)00000-0000"
+    }) // Modelo de validação para o telefone
+})
